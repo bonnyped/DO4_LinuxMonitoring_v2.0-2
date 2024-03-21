4f5d1295304f6ed315837f29c37d93c0baad96d9
bonnyped <bonnyped@student.21-school.ru>
2024-03-21 12:20:29 +0600

    fix part 4

--- src/04/main.sh
+++ src/04/main.sh
@@ -27,7 +27,7 @@ NC='\033[0m'
 RESPONCE_CODES=(200 201 400 401 403 404 500 501 502 503)
 REQUEST_METHODS=(GET POST PUT PATCH DELETE)
 REMOTE_USER=$(echo $USER)
-TIMESTAMP_MY=$(date | awk '{printf "/"$2"/"$6":"}')
+TIMESTAMP_MY=$(date | awk '{printf "/"$3"/"$4":"}')
 
 if [[ $# -gt 0 ]]; then
         echo -e ${RED}"Скрипт запускается без параметров!"${NC}
@@ -42,7 +42,7 @@ for number in 1 2 3 4 5; do
                 GENERATE_IP=$(shuf -i 0-255 -n 4)
                 REMOTE_ADDR=$(echo $GENERATE_IP | tr " " .)
                 #генерируем дату и время
-                GENERATE_TIMESTAMP="["${DATE_TO_WRITE}${TIMESTAMP_MY}$(date | awk '{printf $4" "$5""$7"00]"}')
+                GENERATE_TIMESTAMP="["${DATE_TO_WRITE}${TIMESTAMP_MY}$(date | awk '{printf $5" """$7"00]"}')
                 #генереруем метод запроса
                 GENERATE_REQUEST=$(shuf -i 0-4 -n 1)
                 REQUEST=${REQUEST_METHODS[$GENERATE_REQUEST]}
